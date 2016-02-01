e26c440553f2b6e61d5fec4e7acd4c8cc27b76d6
Tom Pickard <tcpickard94@gmail.com>
2016-02-01 18:49:03 +0000

    Added a few more 'pages'and installed the shotgun gem for page reload

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+set :session_secret, 'super-secret'
 
 get '/' do
   'Hello!'
+end
+
+get '/secret' do
+  'Secret'
+end
+
+get '/new' do
+  'New page'
+end
+
+get '/old' do
+  'Old Page'
 end
